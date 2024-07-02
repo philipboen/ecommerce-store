3a6791677886dab78f2be29521f199daa6c58660
philipboen <philipboen001@gmail.com>
2024-07-02 13:01:18 +0300

    chore: update billboard aspect ratio

--- components/billboard.tsx
+++ components/billboard.tsx
@@ -9,7 +9,7 @@ export const Billboard: React.FC<BillboardProps> = ({ data }) => {
   return (
     <div className="overflow-hidden rounded-xl p-4 sm:p-6 lg:p-8">
       <div
-        className="relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1]"
+        className="relative aspect-square overflow-hidden rounded-xl bg-cover md:aspect-[2.4/1] lg:aspect-[2.8/1]"
         style={{ backgroundImage: `url(${data?.imageUrl})` }}
       >
         <div className="flex size-full flex-col items-center justify-center gap-y-8 text-center">
